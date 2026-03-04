fe1fa373f9596905b085926838d578c515601d5b
Jamal <mybusineess@gmail.com>
2026-03-04 07:33:29 +0000

    create a save button and make it work with JS

--- index.js
+++ index.js
@@ -2,7 +2,14 @@ let count = 0
 
 // let countEl = document.getElementById("count-btn")
 function incrementF() {
-    count++;
+    count += 1;
+    let countEl = document.getElementById("count-btn")
+    countEl.innerText = count
     // countEl.innerHTML = count
+
+}
+
+// 1. Create a function, save(), which logs out the count when it's called
+function save() {
     console.log(count)
 }
