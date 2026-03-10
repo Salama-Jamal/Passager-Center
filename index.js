a32848df7521a7eeaee2e923173b3066a3b2f304
Jamal <mybusineess@gmail.com>
2026-03-10 07:07:44 +0000

    finished passenger app

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 let count = 0;
 function incrementF() {
     count += 1;
-    document.getElementById("btn btn-increment").textContent = count;
+    document.getElementById("btn-increment").textContent = count;
 }
 
 function save() {
     countTxt = count + " - ";
-    document.getElementById("btn btn-save").textContent += countTxt;
+    document.getElementById("btn-save").textContent += countTxt;
     count = 0
+
 }
